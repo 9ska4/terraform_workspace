d52210c6cce10fc483155cb637adc891cfae04af
einhar <oskar.sadlowski@gmail.com>
2021-11-20 21:50:57 +0100

    add load balanced

--- 01-farmconfig/main.tf
+++ 01-farmconfig/main.tf
@@ -20,6 +20,9 @@ resource "aws_autoscaling_group" "example" {
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnet_ids.default.ids
 
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
   max_size = 10
   min_size = 2
 
@@ -52,4 +55,93 @@ data "aws_vpc" "default" {
 
 data "aws_subnet_ids" "default" {
   vpc_id = data.aws_vpc.default.id
+}
+
+// Load balancer declaration
+resource "aws_lb" "example" {
+  name = "terraform-lb-asg-example"
+  load_balancer_type = "application"
+  subnets = data.aws_subnet_ids.default.ids
+  security_groups = [aws_security_group.alb.id]
+}
+
+// Listener for load balancer on :80
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port = 80
+  protocol = "HTTP"
+
+  // return default value (if not match)
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found (custom page)"
+      status_code = "404"
+    }
+  }
+}
+
+// security group for load balancer
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # allow incoming HTTP requests
+  ingress {
+    from_port = 80
+    protocol  = "tcp"
+    to_port   = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # allow outgoing HTTP requests
+  egress {
+    from_port = 0
+    protocol  = "-1"
+    to_port   = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+// Target group (TG) declaration based
+// Intergrated with ASG via `aws_autoscaling_group.{name}.target_group_arns` field (that's why TG knows which EC2 instances should be touched)
+resource "aws_lb_target_group" "asg" {
+  name = "terraform-tg-example"
+  port = var.server_port
+  protocol = "HTTP"
+  vpc_id = data.aws_vpc.default.id
+
+  health_check {
+    path = "/"
+    protocol = "HTTP"
+    matcher = "200"
+    interval = 15
+    timeout = 3
+    healthy_threshold = 2
+    unhealthy_threshold = 2
+  }
+}
+
+// Listener to load balancer - entrance door
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn = aws_lb_listener.http.arn
+  priority = 100
+
+  // every match
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  // forward to loag balancer ARG
+  action {
+    type = "forward"
+    target_group_arn = aws_lb_target_group.asg.arn
+  }
+}
+
+output "alb_dns_name" {
+  description = "Load balanced DNS"
+  value = aws_lb.example.dns_name
 }
